34a2a46d6418df548385853ce428c668327b0e0f
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-03-03 03:53:07 -0800

    chore: remove redux-persist

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,6 @@ import { createStore, applyMiddleware } from 'redux';
 import { createEpicMiddleware } from 'redux-observable';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createLogger } from 'redux-logger';
-import { PersistGate } from 'redux-persist/es/integration/react';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import 'bulma/css/bulma.css';
 import 'react-tippy/dist/tippy.css';
 import 'normalize.css';
@@ -35,15 +32,9 @@ const logger = createLogger({
 });
 middlewares = [...middlewares, logger];
 
-const config = {
-  key: 'state',
-  storage,
-};
-
-const reducer = persistReducer(config, rootReducer);
 
 export const store = createStore(
-  reducer,
+  rootReducer,
 
   // debug
   composeWithDevTools(
@@ -54,30 +45,15 @@ export const store = createStore(
 epicMiddleware.run(rootEpic);
 
 
-const persistor = persistStore(store);
-
-
-
-
-const onBeforeLift = () => {
-
-};
-
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate
-      loading={null}
-      onBeforeLift={onBeforeLift}
-      persistor={persistor}
-    >
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={TyHome} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={TyHome} />
 
-          <Redirect to="/" />
-        </Switch>
-      </BrowserRouter>
-    </PersistGate>
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );

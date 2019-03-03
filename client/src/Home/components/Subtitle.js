79b73f648f7414ee316e518076aa670c545bf01f
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-03-02 17:28:57 -0800

    feat: speech to text

--- client/src/Home/components/Subtitle.js
+++ client/src/Home/components/Subtitle.js
@@ -1,40 +1,154 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+import { Config } from '../../config';
 
 
-function Subtite(props) {
-  const {
-    user,
-    getUser,
-  } = props;
+function Subtitle() {
+  const [partText, setPartText] = useState('');
+  const [allText, setAllText] = useState('');
+  const socket = io(Config.socketUrl);
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
 
-  function onGetUser() {
-    getUser({ id: '0' });
+  let text = '';
+  const bufferSize = 2048;
+  let context;
+  let processor;
+  let input;
+  let stream;
+  let streamStreaming = false;
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      socket.emit('join', 'Server Connected to Client');
+    });
+
+    socket.on('speechData', (data) => {
+      if (data.results == null) return;
+
+      console.log('data', data);
+      const isFinalText = data.results[0].isFinal;
+
+      const newPartText = getPartText(data);
+      setPartText(newPartText);
+
+      if (isFinalText) {
+        text = `${text} ${newPartText}`;
+        setPartText('');
+        setAllText(text);
+      }
+    });
+
+    return () => {
+      handleStopRecording();
+
+      if (streamStreaming) {
+        socket.emit('endGoogleCloudStream', '');
+      }
+    };
+  }, []);
+
+
+  async function initRecording() {
+    socket.emit('startGoogleCloudStream', ''); //init socket Google Speech Connection
+    streamStreaming = true;
+    context = new AudioContext();
+    processor = context.createScriptProcessor(bufferSize, 1, 1);
+    processor.connect(context.destination);
+    context.resume();
+
+    stream = await navigator.mediaDevices
+      .getUserMedia({
+        audio: true,
+        video: false
+      });
+
+
+    input = context.createMediaStreamSource(stream);
+    input.connect(processor);
+
+    processor.onaudioprocess = (e) => {
+      microphoneProcess(e);
+    };
   }
 
-  function onUpdateName() {
-    const { updateName } = props;
+  function microphoneProcess(e) {
+    const left = e.inputBuffer.getChannelData(0);
+    const left16 = downsampleBuffer(left, 44100, 16000);
+    socket.emit('binaryData', left16);
+  }
 
-    updateName({
-      id: '0',
-      name: 'random'
+  function onStartRecording() {
+    // startButton.disabled = true;
+    // endButton.disabled = false;
+    initRecording();
+  }
+
+  function handleStopRecording() {
+    // waited for FinalWord
+    // startButton.disabled = false;
+    // endButton.disabled = true;
+    streamStreaming = false;
+    socket.emit('endGoogleCloudStream', '');
+
+    let track = stream.getTracks()[0];
+    track.stop();
+
+    input.disconnect(processor);
+    processor.disconnect(context.destination);
+    context.close().then(function () {
+      input = null;
+      processor = null;
+      context = null;
     });
   }
 
-  const { name } = user;
+
+  function getPartText(speechData) {
+    return speechData.results.map(r => r.alternatives[0].transcript).join(' ');
+  }
+
+  function downsampleBuffer(buffer, sampleRate, outSampleRate) {
+    if (outSampleRate === sampleRate) {
+      return buffer;
+    }
+
+    if (outSampleRate > sampleRate) {
+      throw 'downsampling rate show be smaller than original sample rate';
+    }
+
+    const sampleRateRatio = sampleRate / outSampleRate;
+    const newLength = Math.round(buffer.length / sampleRateRatio);
+    const result = new Int16Array(newLength);
+    let offsetResult = 0;
+    let offsetBuffer = 0;
+
+    while (offsetResult < result.length) {
+      const nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
+      let accum = 0;
+      let count = 0;
+      for (let i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
+        accum += buffer[i];
+        count++;
+      }
+
+      result[offsetResult] = Math.min(1, accum / count)*0x7FFF;
+      offsetResult++;
+      offsetBuffer = nextOffsetBuffer;
+    }
+    return result.buffer;
+  }
 
   return (
     <div>
-      <button onClick={onGetUser}>
-        Get User
-      </button>
+      <div className="wrapper">
+        <button className="button" type="button" onClick={onStartRecording}>Start</button>
+        <button className="button" type="button" onClick={handleStopRecording}>Stop</button>
+      </div>
 
-      <button  onClick={onUpdateName}>
-        Update Name
-      </button>
-
-      <div>name: {name}</div>
+      {allText} {partText}
     </div>
   );
 }
 
-export default Subtite;
+export default Subtitle;
